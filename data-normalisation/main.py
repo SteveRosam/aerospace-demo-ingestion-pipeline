197e037a5b7df41ea019fcdaa8bcc3ef782bd37e
Javier Blanco <javier+quixers@quix.io>
2025-09-19 11:47:20 +0000

    Edited 'main.py'

--- data-normalisation/main.py
+++ data-normalisation/main.py
@@ -12,7 +12,7 @@ def main():
 
     # Setup necessary objects
     app = Application(
-        consumer_group="data-norm-v1-dev2",
+        consumer_group="data-norm-v1-dev3",
         auto_create_topics=True,
         auto_offset_reset="earliest"
     )

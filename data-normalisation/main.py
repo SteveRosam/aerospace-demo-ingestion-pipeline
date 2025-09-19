5a717b882a6bb34c58ecec930d717aa28221044a
Javier Blanco <javier+quixers@quix.io>
2025-09-19 11:38:14 +0000

    Edited 'main.py'

--- data-normalisation/main.py
+++ data-normalisation/main.py
@@ -22,7 +22,7 @@ def main():
 
     # Do StreamingDataFrame operations/transformations here
     sdf = sdf.print(metadata=True)
-    sdf = sdf["data"].apply(lambda row: row, expand=True)
+    sdf = sdf.apply(lambda row: row, expand=True)
     sdf = sdf.print(metadata=True)
 
     # Finish off by writing to the final result to the output topic

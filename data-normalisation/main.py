3f5f5aa9c29add953c309c30685902ec1afff799
Javier Blanco <javier+quixers@quix.io>
2025-09-19 11:46:45 +0000

    Edited 'main.py'

--- data-normalisation/main.py
+++ data-normalisation/main.py
@@ -22,10 +22,8 @@ def main():
 
     # Do StreamingDataFrame operations/transformations here
     sdf = sdf.print(metadata=True)
-    print("hey")
     sdf = sdf.apply(lambda row: row["data"], expand=True) 
     sdf = sdf.print(metadata=True)
-    print("oh")
 
     # Finish off by writing to the final result to the output topic
     #sdf.to_topic(output_topic)
